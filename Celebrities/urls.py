794e03d95fb700829d0ee8740097042113d457aa
Corentin NICODEME <corentin.nicodeme@telecom-sudparis.eu>
2025-04-19 19:15:06 +0200

    ajout de célébrités

--- Celebrities/urls.py
+++ Celebrities/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("celebs/", include("celebs.urls")),
+    path('', RedirectView.as_view(url='/celebs/', permanent=False)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
